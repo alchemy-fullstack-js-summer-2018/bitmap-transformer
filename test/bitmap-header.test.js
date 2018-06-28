4d4b16cf05bfb8d5427e731fcee38022cdf89837
Mario Quintana <pyrros2000@gmail.com>
2018-06-28 10:39:56 -0700

    testing mocha test

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -23,7 +23,7 @@ describe('bitmap header', () => {
     });
 
     it('parses header data', () => {
-        // TODO: use the constants to populate the following properties
+        // TODONE: use the constants to populate the following properties
         // on the BitmapHeader in its constructor.
         // These test values are correct for the supplied test-bitmap.bmp
         const header = new BitmapHeader(buffer);
