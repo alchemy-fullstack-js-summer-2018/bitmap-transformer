ba6ca6d689eba301b5858cd59c5234694f6070bd
Mark Myers <markalope@gmail.com>
2018-06-28 09:31:02 -0700

    Added third grayscale test

--- test/transformers.test.js
+++ test/transformers.test.js
@@ -7,15 +7,16 @@ describe('transformers', () => {
     it('invert', () => {
         // HINT: invert subtracts each value from 255
         const transformed = invert({
-            r: 34,
+            
+            b: 205,
             g: 100,
-            b: 205
+            r: 34
         });
 
         assert.deepEqual(transformed, {
-            r: 221,
+            b: 50,
             g: 155,
-            b: 50
+            r: 221
         });
     });
 
@@ -23,17 +24,32 @@ describe('transformers', () => {
         // HINT: grayscale assigns the average of all three colors
         // as the new value for each color
         const transformed = grayscale({
-            r: 34,
+            b: 205,
             g: 100,
-            b: 205
+            r: 34
         });
 
         assert.deepEqual(transformed, {
-            r: 113,
+            b: 113,
             g: 113,
-            b: 113
+            r: 113
         });
     });
 
     // TODO: add a third transformer (you'll need to add the module and require!) and test
+    it('grayscale luminosity', () => {
+        // HINT: uses luminositry formula to determine
+        // the new grey value for each color 0.21 R + 0.72 G + 0.07 B
+        const transformed = grayscale({
+            r: 34, // 7
+            g: 100, // 72
+            b: 205 // 14
+        });
+
+        assert.deepEqual(transformed, {
+            r: 31,
+            g: 31,
+            b: 31
+        });
+    });
 });
