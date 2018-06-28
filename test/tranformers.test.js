8bcf975c7f9ffa0a0f638267828ea883acef3671
Carmen V Ramos <carmenvramos@yahoo.com>
2018-06-28 10:57:00 -0700

    Add Luminosity trasformer & Pass test

--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { invert } = require('../lib/invert-transformer');
 const { grayscale } = require('../lib/grayscale-transformer');
+const { luminosity } = require('../lib/luminosity-transformer');
 
 describe('transformers', () => {
 
@@ -36,4 +37,18 @@ describe('transformers', () => {
     });
 
     // TODO: add a third transformer (you'll need to add the module and require!) and test
+
+    it('luminosity', () => {
+
+        const transformed = luminosity({
+            r: 34,
+            g: 100,
+            b: 205
+        });
+        assert.deepEqual(transformed, {
+            r: 93,
+            g: 93,
+            b: 93
+        });
+    });
 });
