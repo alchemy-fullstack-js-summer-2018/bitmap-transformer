166612eb0b3838c06d448044d29146cec8985105
Mark Myers <markalope@gmail.com>
2018-06-28 16:21:12 -0700

    started setting up streaming file and test file

---
+++ test/stream-transformer.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const getBitmapHeader = require('../lib/stream-transformer');
+const fs = require('fs');
+
+const readText = file => fs.readFileSync(file, 'utf8');
+
+
+
+
