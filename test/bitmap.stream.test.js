ae2c32511d9a9475aac43ef6b13b527b400033f5
Mario Quintana <pyrros2000@gmail.com>
2018-06-28 16:55:16 -0700

    copied our header test for our streaming tets

--- test/bitmap.stream.test.js
+++ test/bitmap.stream.test.js
@@ -1,2 +1,25 @@
 const assert = require('assert');
 const { readFile } = require('fs').promises;
+const constants = require('../lib/bitmap-constants');
+const BitmapHeader = require('../lib/bitmap-header');
+
+describe('bitmap header', () => {
+
+    let buffer = null;
+    beforeEach(() => {
+        return readFile('./test/test-bitmap.bmp')
+            .then(b => buffer = b);
+    });
+    it('has correct specs', () => {
+        assert.ok(constants.PIXEL_OFFSET);
+        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
+        assert.ok(constants.FILE_SIZE_OFFSET);
+    });
+
+    it('parses header data', () => {
+        const header = new BitmapHeader(buffer);
+        assert.equal(header.pixelOffset, 54);
+        assert.equal(header.bitsPerPixel, 24);
+        assert.equal(header.fileSize, 30054);
+    });
+});
