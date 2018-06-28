8c7c04a2feca5f890e970aae0c8529e3b10787e1
Mario Quintana <pyrros2000@gmail.com>
2018-06-27 17:31:22 -0700

    starting to work on bitmap test

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { readFile } = require('fs').promises;
 const constants = require('../lib/bitmap-constants');
 const BitmapHeader = require('../lib/bitmap-header');
 
@@ -7,12 +8,17 @@ describe('bitmap header', () => {
     let buffer = null;
     beforeEach(() => {
         // TODO: file read './test/test-bitmap.bmp' and put the promise return into buffer variable
+        return readFile('./test/test-bitmap.bmp')
+            .then(b => buffer = b);
+    
     });
 
     it('has correct specs', () => {
         // TODO: read the wiki spec docs to figure out what these values should be.
         // You don't need to change this test, you need to put the correct values into
         // '../lib/bitmap-constants'
+
+
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
