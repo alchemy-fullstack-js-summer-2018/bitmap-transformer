5aaf4676a08d18e78125fea1c6a32f1a91d62954
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-06-28 17:06:55 -0700

    Make test for stream-transformer

--- test/stream-transformer.test.js
+++ test/stream-transformer.test.js
@@ -1,7 +1,21 @@
 const assert = require('assert');
-const getBitmapHeader = require('../lib/stream-transformer');
-const fs = require('fs');
+const StreamingBitmapTransformer = require('../lib/stream-transformer');
+const { readFile } = require('fs').promises;
+const invert = require('../lib/invert-transformer');
 
-const readText = file => fs.readFileSync(file, 'utf8');
+describe.only('bitmap file transformer', () => {
 
+    const source = readFile('./test-bitmap.bmp');
 
+    it('test whole transform', () => {
+
+        const bitmap = new StreamingBitmapTransformer(source);
+        bitmap.transform(invert);
+
+        return readFile('./test/inverted-expected.bmp')
+            .then(() => {
+                const expected = readFile('./inverted-expected.bmp');
+                assert.equal(source, expected);
+            });
+    });
+});
