c14e4308f60746df5ac5dca4106669b77a2ac389
carriecarter <carrie@carrie-carter.com>
2018-07-02 10:33:47 -0700

    get bitmap header complete

--- test/get-bitmap-header.test.js
+++ test/get-bitmap-header.test.js
@@ -1,22 +1,16 @@
 const assert = require('assert');
-const { readFile } = require('fs').promises;
-const BitmapTransformer = require('../lib/bitmap-transformer');
 const getBitmapHeader = require('../lib/get-bitmap-header');
+const { join } = require('path');
 
 describe('get bitmap header', () => {
-//a function that takes a file name as an input
-    function readFrom(file, length, position = 0) {
-        const buffer = Buffer.alloc(length);
-        return open(file, 'r')
-        // fd === "file descriptor"
-        .then(fd => read(fd, buffer, 0, length, position))
-        .then(contents => contents.buffer);
-        // returns a promise that resolves to an object with 
-        // the header properties: Pixel offset, bitsPerPixel, fileSize
-        
+    const source = join(__dirname, 'test-bitmap.bmp');
+
+    it('gets bitmap header details', () => {
+        return getBitmapHeader(source)
+            .then(header => {
+                assert.equal(header.pixelOffset, 54);
+                assert.equal(header.bitsPerPixel, 24);
+                assert.equal(header.fileSize, 30054);
+            });
     });
-    
-    const file = './test/test-bitmap.bmp';
-    const length = 10;
-    const position = 5;
-    
+});
