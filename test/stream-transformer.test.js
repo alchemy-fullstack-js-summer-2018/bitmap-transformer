610d18fde1c479d5697db916edd6bbd81ca43583
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-02 10:41:00 -0700

    Add separate readFrom function and header test

--- test/stream-transformer.test.js
+++ test/stream-transformer.test.js
@@ -1,15 +1,15 @@
 const assert = require('assert');
-const StreamingBitmapTransformer = require('../lib/stream-transformer');
+// const StreamingBitmapTransformer = require('../lib/stream-transformer');
 const { readFile } = require('fs').promises;
 const invert = require('../lib/invert-transformer');
 
-describe.only('bitmap file transformer', () => {
+describe('bitmap file transformer', () => {
     const actualFile = './test-bitmap.bmp';
 
     let source = null;
-    beforeEach(() => {
-        source = new StreamingBitmapTransformer('./test-bitmap.bmp');
-    });
+    // beforeEach(() => {
+    //     source = new StreamingBitmapTransformer('./test-bitmap.bmp');
+    // });
 
     it('test whole transform', () => {
         return source.transform(invert, actualFile)
