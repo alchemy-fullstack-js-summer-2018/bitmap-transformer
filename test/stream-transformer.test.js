517022fd02ce5014bf48a618dd5ce29656f60310
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-02 15:28:58 -0700

    Begin implementing transform method and test

--- test/stream-transformer.test.js
+++ test/stream-transformer.test.js
@@ -1,22 +1,36 @@
 const assert = require('assert');
-// const StreamingBitmapTransformer = require('../lib/stream-transformer');
-const { readFile } = require('fs').promises;
+const StreamingBitmapTransformer = require('../lib/stream-transformer');
+// const getBitmapHeader = require('../lib/getBitmapHeader');
 const invert = require('../lib/invert-transformer');
+// const { readFile } = require('fs').promises;
+// const invert = require('../lib/invert-transformer');
 
 describe('bitmap file transformer', () => {
-    const actualFile = './test-bitmap.bmp';
-
-    let source = null;
+    const actualFile = 'test-bitmap.bmp';    
     // beforeEach(() => {
-    //     source = new StreamingBitmapTransformer('./test-bitmap.bmp');
+    //     source = new StreamingBitmapTransformer(actualFile, getBitmapHeader(actualFile)
+    //         .catch(err => {
+    //             if(err.code !== 'ENOENT') throw err;
+    //         })
+    //         .then(result => console.log(result))
+    //     );
     // });
 
-    it('test whole transform', () => {
-        return source.transform(invert, actualFile)
-            .then(() => {
-                const actual = readFile(actualFile);
-                const expected = readFile('./inverted-expected.bmp');
-                assert.equal(actual, expected);
+
+    it('runs the transform function', () => {
+        return StreamingBitmapTransformer.create(actualFile)
+            .catch(err => {
+                if(err.code !== 'ENOENT') throw err;
+            })
+            .then(bitmapTransformer => {
+                return bitmapTransformer.transform(invert, actualFile)
+                    .then(result => console.log(result));
             });
+        // return source.transform(invert, actualFile)
+        //     .then(() => {
+        //         const actual = actualFile;
+        //         const expected = './inverted-expected.bmp';
+        //         assert.equal(actual, expected);
+        //     });
     });
 });
