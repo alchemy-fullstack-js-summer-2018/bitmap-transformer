4bd9d3b9a9abe809285180d3042c47ad3b1705ac
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-03 10:41:28 -0700

    appended actual-inverted.bmp

--- test/stream-bitmap-transformer.test.js
+++ test/stream-bitmap-transformer.test.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const { readFile } = require('fs').promises;
 const StreamingBitmapTransformer = require('../lib/stream-bitmap-transformer');
 const invert = require('../lib/invert-transformer');
-const getBitmapHeader = require('../get-bitmap-header');
+const getBitmapHeader = require('../lib/get-bitmap-header');
 
 const readText = file => fs.readFileSync(file, 'utf8');
 
 describe('transforming the stream', () => {
     let transformer = null;
     beforeEach(() => {
-        transformer = new StreamingBitmapTransformer('./test/test-bitmap.bmp');
+        transformer = new StreamingBitmapTransformer('./test/test-bitmap.bmp', getBitmapHeader);
     }); 
 
     it('inverts the stream', () => {
-        return transformer.transform(invert, './test/actual-inverted.bmp')
+        return transformer.transform('./test/actual-inverted.bmp')
             .then(() => {
                 const actual = readText('./test/actual-inverted.bmp');
                 const expected = readText('./test/inverted.expected.bmp');
